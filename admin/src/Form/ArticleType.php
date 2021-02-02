85582c2037c14f87bd9f5ef8debd82ee093f41dd
Enkhzaya.O <enkhzaya.o@gmobile.mngit>
2021-02-02 19:00:01 +0800

    02.02

--- admin/src/Form/ArticleType.php
+++ admin/src/Form/ArticleType.php
@@ -4,6 +4,11 @@ namespace App\Form;
 
 use App\Entity\Article;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,17 +17,26 @@ class ArticleType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('title')
+            ->add('title',TextType::class, array('label' => 'Гарчиг'))
             ->add('bodys')
-            ->add('picture')
+            ->add('picture', FileType::class,array('label' => 'Зураг') )
             ->add('types')
-            ->add('status')
-            ->add('views')
-            ->add('date')
-            ->add('comments')
-            ->add('sort')
-            ->add('who')
-            ->add('createdDate')
+            ->add('status',ChoiceType::class, [
+                'choices' => [
+                    'Тийм' => 1,
+                    'Үгүй' => 0,
+                ],
+                'multiple' => false,
+                'label' => 'Хэрэглэгчдэд харагдах эсэх'])
+//            ->add('views')
+            ->add('date', DateType::class,[
+                'widget' => 'single_text',
+                'html5' => true,
+            ])
+            ->add('comments',TextType::class, array('label' => 'Товч тайлбар'))
+            ->add('sort',NumberType::class, array('label' => 'Эрэмбэ'))
+//            ->add('who')
+//            ->add('createdDate')
         ;
     }
 
